9c1dbdb4e7038f9fea070625c82a1dda517864ef
Rickcolan100 <90806086+Rickcolan100@users.noreply.github.com>
2022-04-08 10:54:52 -0300

    Add files via upload

--- ej4p2.py
+++ ej4p2.py
@@ -11,32 +11,32 @@ the Numba, NumPy, and PyTorch packages and the Python C-API to create a scalable
 system that can exploit the largest HPC resources and emerging computing architectures.
 """
 
-contador = [("Titulo", False) , ("Facil" , 0) , ("Medio" , 0) , ("Dificil" , 0) , ("Muy dificil" , 0)]
+contador = [0,0,0,0]
 
 #verifica si el tamaño del doctring con __len__ si es menor a 11 en la linea 1
-contador["Titulo"] = len(texto.splitlines()[0].split("titulo: ")) <= 11 
+titulo = (((texto.split('\n')[0]).split()))
+titulo.remove(titulo[0])
 
 #recorre el resumen y suma el contador
 for elem in texto.split('resumen: ')[1].split('.'):
-     print(elem)
      if len(elem.split()) <= 12:
-         contador['Facil'] += 1
+         contador[0] += 1
      elif len(elem.split()) <= 17:
-         contador['Medio'] += 1
+         contador[1] += 1
      elif len(elem.split()) <= 25:
-         contador['Dificil'] += 1
+         contador[2] += 1
      else:
-         contador['Muy dificil'] += 1
+         contador[3] += 1
 
-if contador['Titulo:']:
+if len(titulo) <= 11:
      print('El titulo del texto esta bien')
 else:
      print('El titulo del texto es demasiado largo')
 
 print(f"""Cantidad de oraciones de oraciones: 
-          - Faciles de leer: {contador['Facil']} , 
-          - Media de leer: {contador['Medio']}, 
-          - Dificil de leer: {contador['Dificil']} , 
-          - Muy dificl de leer: {contador ['Muy dificil']}""")    
+          - Faciles de leer: {contador[0]}  
+          - Media de leer: {contador[1]}
+          - Dificil de leer: {contador[2]}  
+          - Muy dificl de leer: {contador [3]}""")    
 
 
